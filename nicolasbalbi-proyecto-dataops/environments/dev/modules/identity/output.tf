d06b102f70ea705fd0a3f59a52e9131a17d9a810
n1c021 <79662003+n1c021@users.noreply.github.com>
2026-08-18 22:56:57 -0300

    Delete nicolasbalbi-proyecto-dataops directory
    Restructura del repositorio

--- nicolasbalbi-proyecto-dataops/environments/dev/modules/identity/output.tf
+++
@@ -1,8 +0,0 @@
-output "role_arn" {
-  value       = aws_iam_role.data_processing_role.arn
-  description = "ARN del Rol IAM configurado"
-}
-output "role_name" {
-  value       = aws_iam_role.data_processing_role.name
-  description = "Nombre del Rol IAM"
-}
